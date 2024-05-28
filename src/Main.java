b3c88ee0e0898f37d1e8df093f0829a82b2cbb58
Sloth <austere4@gmail.com>
2024-05-28 11:37:18 +0300

    MilesBonus prog added

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 100;
+        int step = 20;
+        int miles = service.calculate(price, step); // должно получиться 500
+        System.out.println(miles);
+    }
 }
